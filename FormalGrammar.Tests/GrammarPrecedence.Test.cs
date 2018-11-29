9b8254ef45c613215c47bb0bc0c0ed921f5ec88b
Anton Tolstov <anaym.ru@gmail.com>
2018-11-29 17:41:54 +0500

    GrammarPrecedence.Test

---
+++ FormalGrammar.Tests/GrammarPrecedence.Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using FluentAssertions;
+using FormalGrammar.Model;
+using FormalGrammar.Utils;
+using NUnit.Framework;
+
+namespace FormalGrammar.Tests
+{
+    [TestFixture]
+    public class GrammarPrecedence_Should
+    {
+        [Test]
+        public static void CorrectFindLessAndEqualAndGreateSets_WhenG31()
+        {
+            var grammar = GrammarParser.Parse("S aFSb", "S c", "F Fb", "F b");
+            
+            var precedence = new GrammarPrecedence(grammar);
+
+            precedence.Less.Select(p => (p.Item1.Value, p.Item2.Value))
+                .Should()
+                .BeEquivalentTo(('a', 'F'), ('a', 'b'), ('F', 'a'), ('F', 'c'));
+
+            precedence.Equal.Select(p => (p.Item1.Value, p.Item2.Value))
+                .Should()
+                .BeEquivalentTo(('a', 'F'), ('F', 'S'), ('S', 'b'), ('F', 'b'));
+
+            precedence.Greater.Select(p => (p.Item1.Value, p.Item2.Value))
+                .Should()
+                .BeEquivalentTo(('b', 'a'), ('b', 'c'), ('b', 'b'), ('c', 'b'));
+        }
+    }
+}
